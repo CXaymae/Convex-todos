3328fb1cf304be83e9ff75ce74a415cd68212f6c
CXaymae <morabiitchaima@gmail.com>
2024-09-14 17:28:53 +0200

    Fix: infinite loop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 type ToDoItem = {
   title: string;
@@ -10,53 +10,59 @@ type ToDoItem = {
 
 export default function Home() {
   const [todos, setTodos] = useState<ToDoItem[]>([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-const [title, setTitle] = useState("");
-const [description, setDescription] = useState("");
-
-const handleSubmit = (e: React.FormEvent<HTMLElement>) => (
-  e.preventDefault());
-  setTodos(prev => {
-    const newTodos = [...prev];
-    newTodos.push({title, description, completed: false});
-    return newTodos;
-  });
-
-  setTitle("");
-  setDescription("");
-
+  const handleSubmit = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    setTodos(prev => {
+      const newTodos = [...prev];
+      newTodos.push({ title, description, completed: false });
+      return newTodos;
+    });
+    setTitle("");
+    setDescription("");
+  };
 
   return (
     <div className="max-w-screen-md mx-auto p-4">
       <h1 className="text-xl font-bold">To Do List</h1>
       <ul>
-      {todos.map(({ title, description, completed}, index) => ( 
-        <li key={index}>
-         <input 
-         type="checkbox" 
-         checked={completed} 
-         onChange={e => setTodos(prev => {
-          const newTodos = [...prev];
-          prev[index].completed = e.target.checked;
-          return prev;
-         })} 
-        />
-         <span className="font-semibold">{title}</span>
-         {description}
-        </li>
-        
-      ))}
+        {todos.map(({ title, description, completed }, index) => (
+          <li key={index}>
+            <input
+              type="checkbox"
+              checked={completed}
+              onChange={e => setTodos(prev => {
+                const newTodos = [...prev];
+                newTodos[index] = { ...newTodos[index], completed: e.target.checked };
+                return newTodos;
+              })}
+            />
+            <span className="font-semibold">{title}</span>
+            {description}
+          </li>
+        ))}
       </ul>
       <form onSubmit={handleSubmit}>
         <label htmlFor="title">Title</label>
-        <input type="text" name="title" id="title" value={title} onChange={e => setTitle(e.target.title)}/>
+        <input
+          type="text"
+          name="title"
+          id="title"
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+        />
         <label htmlFor="description">Description</label>
-        <input type="text" name="description" id="description" value={description} onChange={e => setDescription(e.target.value)}/>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          value={description}
+          onChange={e => setDescription(e.target.value)}
+        />
         <button type="submit">Create</button>
       </form>
-
-      
     </div>
-    
   );
 }
