c4c9533d406ab3d3d3d0e2f6fec2dfd6301e5eec
CXaymae <morabiitchaima@gmail.com>
2024-09-14 14:25:35 +0200

    UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
 "use client";
 
+import {useState} from "react";
+
+type ToDoItem = {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
 export default function Home() {
+  const [todos, setTodos] = useState<ToDoItem[]>([]);
   return (
     <div className="max-w-screen-md mx-auto p-4">
       <h1 className="text-xl font-bold">To Do List</h1>
+      <ul>
+      {todos.map(({ title, description, completed}, index) => ( 
+        <li key={index}>
+         <input 
+         type="checkbox" 
+         checked={completed} 
+         onChange={e => setTodos(prev => {
+          const newTodos = [...prev];
+          prev[index].completed = e.target.checked;
+          return prev;
+         })} 
+        />
+         <span className="font-semibold">{title}</span>
+         {description}
+        </li>
+        
+      ))}
+      </ul>
+
+      
     </div>
     
   );
